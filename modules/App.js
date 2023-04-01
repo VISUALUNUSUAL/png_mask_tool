e76d985c05c0e8613926a3b2ca2523ad795b70d0
VU <105385142+VISUALUNUSUAL@users.noreply.github.com>
2023-04-01 23:38:49 +0530

    Update App.js

--- modules/App.js
+++ modules/App.js
@@ -1,16 +1,16 @@
 class Mask {
     constructor(srcPhoto, maxSize) {
+        
         this.rollover = false; // Is the mouse over the area?\
         this.srcPhoto = srcPhoto;
         this.ratio = srcPhoto.width / srcPhoto.height;
         this.maxSize = maxSize;
         this.scale = 1;
-
+        
         this.w = this.maxSize;
         this.h = this.maxSize;
         this.x = width / 2 - this.w / 2;
         this.y = height / 2 - this.h / 2;
-
         this.updSize(this.scale);
 
         this.photo = createImage(this.w, this.h);
@@ -24,7 +24,6 @@ class Mask {
         if (appMode == 'Scale') {
             this.tmask = createImage(this.w, this.h);
             this.tmask.copy(this.mask, 0, 0, this.w, this.h, 0, 0, this.w, this.h);
-
         } else {
             this.mask = createGraphics(this.w, this.h);
             this.mask.copy(this.tmask, 0, 0, this.tmask.width, this.tmask.height, 0, 0, this.w, this.h);
@@ -34,7 +33,6 @@ class Mask {
     updPos() {
         this.x += (this.pw - this.w) / 2;
         this.y += (this.ph - this.h) / 2;
-
         if (this.dragging) {
                 this.x = mouseX + this.offsetX;
                 this.y = mouseY + this.offsetY;
@@ -45,7 +43,6 @@ class Mask {
         this.scale = s;
         this.pw = this.w;
         this.ph = this.h;
-
         if (this.ratio < 1) {
             this.w = Math.round(this.maxSize * this.ratio * this.scale);
             this.h = Math.round(this.maxSize * this.scale);
@@ -62,8 +59,6 @@ class Mask {
                 this.dragging = true;
                 this.offsetX = this.x - mouseX;
                 this.offsetY = this.y - mouseY;
-                
-
             } else {
                 this.brushSize = brushSize;
                 this.mask.stroke(240, 62, 62);
@@ -103,7 +98,6 @@ class Mask {
             img.pixels[i + 3] = 255 - v;
         }
         img.updatePixels();
-
         //convert _content from pg to image
         var contentImg = createImage(_content.width, _content.height);
         contentImg.copy(_content, 0, 0, _content.width, _content.height, 0, 0, _content.width, _content.height);
@@ -123,6 +117,4 @@ class Mask {
         this.x = width / 2 - this.w / 2;
         this.y = height / 2 - this.h / 2;
     }
-
-
 }
